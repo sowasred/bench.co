86b173e6f1e7db7769f128b3133b392ffacd9a19
sowasred <muldurozan@gmail.com>
2020-12-06 21:30:52 -0500

    main.js cleaned

--- main.js
+++ main.js
@@ -26,8 +26,8 @@ async function fetchMetaData() {
 }
 
 function clearTable(table) {
-  var rows = table.rows;
-  var i = rows.length;
+  let rows = table.rows;
+  let i = rows.length;
   while (--i) {
     rows[i].parentNode.removeChild(rows[i]);
   }
@@ -43,10 +43,6 @@ const currecyFormat = (amount) => {
 const loadResult = (array) => {
   let tableSelf = document.querySelector("table");
   clearTable(tableSelf);
-  // let tablebody = document.querySelector("tbody");
-
-  // console.info("ozan", tablebody);
-  // tableSelf.appendChild(tablebody);
   calculateTotal(tableSelf, array);
   generateTable(tableSelf, array);
 };
@@ -56,12 +52,10 @@ fetchMetaData();
 function generateTable(table, data) {
   for (let element of data) {
     let newRow = table.insertRow(-1);
-
     let tbody = document.createElement("tbody");
 
     for (let key in element) {
       let newText = document.createTextNode(element[key]);
-
       if (key == "Date") {
         let newCell = newRow.insertCell(0);
         newCell.appendChild(newText);
@@ -84,14 +78,8 @@ function generateTable(table, data) {
 
 const calculateTotal = (table, data) => {
   let total = 0;
-  let thead = document.querySelector("#header");
-  console.info(thead);
-
   data.map((item) => {
-    console.info(item);
     total += parseFloat(item.Amount);
   });
-  console.info(total);
-
   table.rows[0].cells[3].innerHTML = currecyFormat(total);
 };
